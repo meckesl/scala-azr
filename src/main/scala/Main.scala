b4d331c78110094f24d02f4b407a5e6b6998fd98
louis.meckes <louis.meckes@gmx.com>
2025-10-04 11:47:40 +0200

    Runs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
-package core
-
 import models.MockLLM
+import core._
 
 object Main extends App {
   val llm = new MockLLM()
--- src/main/scala/utils/PromptLoader.scala
+++ src/main/scala/utils/PromptLoader.scala
@@ -1,10 +1,21 @@
 package utils
 
 import scala.io.Source
+import java.nio.file.{Paths, Files}
 
 object PromptLoader {
   def load(filename: String): String = {
-    val resource = getClass.getResource(s"/prompts/$filename")
-    Source.fromURL(resource).mkString
+    val resourcePath = s"prompts/$filename"
+    val resourceUrl = getClass.getResource(s"/$resourcePath")
+    if (resourceUrl == null) {
+      throw new IllegalArgumentException(
+        s"Prompt file not found: $resourcePath. " +
+          s"Make sure the file exists in src/main/resources/prompts/"
+      )
+    }
+    val s = Source.fromURL(resourceUrl)
+    val r = s.mkString
+    s.close()
+    r
   }
 }
